625e5f1f0f6ce445956e3275dfe9f31ae35fd956
Jinwoo-1162 <jpark955@gatech.edu>
2021-06-29 18:14:46 -0700

    Finished all components without proper backend support

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,53 @@
 <template>
   <div class="container">
-    <Header title="Task Tracker" />
-    <Tasks @delete-task="deleteTask" :tasks="tasks" />
+    <Header :showAddTask="showAddTask" @toggle-add-task="toggleAddTask" title="Task Tracker" />
+    <div v-show="showAddTask">
+      <AddTask @add-task="addTask" />
+    </div>
+    <Tasks @toggle-reminder="toggleReminder" @delete-task="deleteTask" :tasks="tasks" />
   </div>
 </template>
 
 <script>
 import Header from './components/Header.vue';
 import Tasks from './components/Tasks.vue';
+import AddTask from './components/AddTask.vue';
 
 export default {
   name: 'App',
   components: {
     Header,
     Tasks,
+    AddTask,
   },
   data() {
     return {
-      tasks: []
+      tasks: [],
+      showAddTask: false,
     }
   },
   methods: {
+    toggleAddTask() {
+      this.showAddTask = !this.showAddTask;
+    },
+    addTask(task) {
+      this.tasks = [...this.tasks, task];
+    },
     deleteTask(id) {
-      this.tasks = this.tasks.filter((task) => task.id !== id);
+      if (confirm('Are you sure?')) {
+        this.tasks = this.tasks.filter((task) => {
+          return task.id !== id
+        });
+      }
+    },
+    toggleReminder(id) {
+      this.tasks = this.tasks.map((task) => {
+        if (task.id === id) {
+          return {...task, reminder: !task.reminder}
+        } else {
+          return task;
+        }
+      })
     }
   },
   created() {
